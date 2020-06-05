28b68104b5d7638e7dbf3eeee64e6547a945d4f2
Manish <manishve@umich.edu>
2020-06-04 23:51:12 -0400

    Update calculate_gstar_2000.R

--- calculate_gstar_2000.R
+++ calculate_gstar_2000.R
@@ -12,49 +12,23 @@ census_api_key = 'your census API key here as a string'
 readRenviron("~/.Renviron")       
 
 
-# Get subject location data
-projString = "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
-
-location.dir = "/subject_location/"
-
-location.original = st_read(location.dir, 'subject_location_conus', stringsAsFactors = FALSE)
-location.original = st_transform(location.original, projString)
-
-# Attach state and county information - be careful about year
-
-# For SF3 table we want counties for the year 2000
-
-county.data = st_read('/tl_2010_us_county00.shp')
-county.data = county.data[c('STATEFP00', 'COUNTYFP00')]
-county.data['STATEFP00'] = as.character(county.data$STATEFP00)
-county.data['COUNTYFP00'] = as.character(county.data$COUNTYFP00)
-
-county.data = st_transform(county.data, projString)
-location.county = st_intersection(location.original, county.data)
-remove(list = c('county.data', 'location.original'))
-
-# We want to calculate for each county at census tract level
-st.county = location.county[c('STATEFP00', 'COUNTYFP00')]
-st_geometry(st.county) = NULL
-uq.st.county = distinct(st.county)
-# uq.st.county['STATEFP10'] = as.character(uq.st.county$STATEFP10)
-# uq.st.county['COUNTYFP10'] = as.character(uq.st.county$COUNTYFP10)
-
-# Income information
-var.name = load_variables(year = '2000', dataset = 'sf3', cache = TRUE)
-index = grepl('Median household income', var.name$label)
-income.var = var.name[index,] # We want fourth 'P053001'
-
-#### 1. Calculate for SF3 table from Census 2000 (I have obtained results with 2010 and 2000 and saved the results)
-error.df = data.frame("GEOID10" = NA, "income"= NA, "geometry"= NA, "gstar"= NA )
-
-error.list = list()
-
-
-calculate.gstar = function (state.fip, county.fip){
+calculate.gstar = function (state.fip, county.fip, census.var){
    
+  # This function calculates G-star.
+  # Arguments:
+  #         state.fip: Two digit state fips code. It is a charactor vector
+  #        county.fip: Three digit county fips code. It is a character vector
+  #         census.var: Code of the tract level census variable ('P053001') 
+  # Returns:
+  #         calculate.gstar: Tract level gstar
+
+  # Example:
+  #         c.g = calculate.gstar ('26', '123','P053001')
+
+   
+   # I am not very careful about tryCatch below. I just want to take care of the known issues. 
    tryCatch({
-   var1 = get_decennial(geography = 'tract', variables = 'P053001', year = 2000 , 
+   var1 = get_decennial(geography = 'tract', variables = census.var, year = 2000 , 
                         state = state.fip, county = county.fip, sumfile = 'sf3', geometry = TRUE)
    
    # For some reason there are empty geometries (e.g. try state = '34' and county = '011'), remove them
@@ -79,59 +53,3 @@ calculate.gstar = function (state.fip, county.fip){
       print (e)
    })
 }
-
-
-applyFun = function (state.fip, county.fip) {
-      
-      try (
-      {t.gstar = calculate.gstar (state.fip, county.fip)}, silent = FALSE)
-      
-      if (class (t.gstar) == 'try-error') {
-      print ('error he')
-      } else {return (t.gstar)}
-      
-}
-
-gstar.tracts = apply(uq.st.county, 1, function (x) applyFun(x[1], x[2]))
-
-# Find the ones where there was an error and so we do not get dataframe
-valid.result = lapply(gstar.tracts, is.data.frame)
-valid.result = do.call(rbind, valid.result)
-print(which(!valid.result)) # There are five without results and have errors
-
-
-gstar.valid = gstar.tracts[valid.result]
-gstar.valid = do.call(rbind, gstar.valid)
-
-gstar.valid = st_transform(gstar.valid, projString)
-
-location.gstar.county = st_intersection(location.original, gstar.valid)
-location.gstar.county = location.gstar.county %>% group_by(move_id) %>% summarise()
-
-# Some move_id do not have gstar data - the possible reasons could be
-# There is only one tract in the county
-# More than one tracts but the tracts are embedded (contained in ) other tracts
-# There was geometry or data error from census site
-# Let us probe a bit
-
-st.county.error = uq.st.county[!valid.result,]
-
-
-
-
-moveid.notcovered = location.county$move_id %in% location.gstar.county$move_id
-moveid.notcovered = location.county[!moveid.notcovered,]
-
-# Let us try again for these moveids
-uq.st.county1 = moveid.notcovered[c('STATEFP10')]
-
-
-
-
-## There are NAs in the results
-
-index = lapply(gstar.tracts, function (x) sum(rowSums(is.na(x)))>0 )
-index = do.call(rbind, index)
-
-
-
